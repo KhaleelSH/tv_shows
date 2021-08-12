54045d1ce1890c95880d1fe877db00587224b055
Khaleel Shaheen <khaleel_sh@hotmail.com>
2021-08-13 02:51:13 +0300

    add show details page UI

---
+++ lib/pages/show_details_page.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:tv_shows/pages/add_episode_page.dart';
+import 'package:tv_shows/pages/episode_details_page.dart';
+import 'package:tv_shows/utils/app_colors.dart';
+import 'package:tv_shows/widgets/adaptive_refresh_indicator.dart';
+import 'package:tv_shows/widgets/back_button_app_bar.dart';
+import 'package:tv_shows/widgets/episode_tile.dart';
+
+class ShowDetailsPage extends StatelessWidget {
+  const ShowDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          AdaptiveRefreshIndicator(
+            onRefresh: () async {
+              await Future.delayed(const Duration(seconds: 5));
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Image.network(
+                        'https://i.ytimg.com/vi/MJuFdpVCcsY/movieposter_en.jpg',
+                        height: MediaQuery.of(context).size.height / 2,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset(
+                            'assets/images/image_placeholder_episodes.png',
+                            height: MediaQuery.of(context).size.height / 2,
+                            width: double.infinity,
+                          );
+                        },
+                      ),
+                      Positioned.fill(
+                        bottom: -1,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.white,
+                                Colors.transparent,
+                              ],
+                              begin: FractionalOffset.bottomCenter,
+                              end: FractionalOffset(0.5, 0.75),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'The Office',
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          'A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        const SizedBox(height: 32),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              'Episodes',
+                              style: Theme.of(context).textTheme.headline2,
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              '19',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline2!
+                                  .copyWith(
+                                      color: Color(AppColors.lightGrey())),
+                            ),
+                          ],
+                        ),
+                        for (int i = 0; i < 10; i++)
+                          EpisodeTile(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (_) => const EpisodeDetailsPage()));
+                            },
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: BackButtonAppBar(),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (_) => const AddEpisodePage(),
+            fullscreenDialog: true,
+          ));
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        tooltip: 'Add episode',
+      ),
+    );
+  }
+}
---
+++ lib/widgets/episode_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class EpisodeTile extends StatelessWidget {
+  const EpisodeTile({Key? key, required this.onTap}) : super(key: key);
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.all(0),
+      onTap: onTap,
+      title: Row(
+        children: [
+          Text(
+            'S02 E06',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(width: 12),
+          const Text(
+            'Episode Title',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+      trailing: const Icon(Icons.keyboard_arrow_right),
+    );
+  }
+}
