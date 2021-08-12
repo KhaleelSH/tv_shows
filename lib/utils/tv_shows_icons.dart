e6377f0f9bebf2560961b7d29a6c5248f07b38aa
Khaleel Shaheen <khaleel_sh@hotmail.com>
2021-08-13 02:43:26 +0300

    add TVShowsIcons dart file

---
+++ lib/utils/tv_shows_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class TVShowsIcons {
+  TVShowsIcons._();
+
+  static const _kFontFam = 'TVShowsIcons';
+
+  static const IconData comments = IconData(0xe800, fontFamily: _kFontFam);
+}
