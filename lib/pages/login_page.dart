5101a9711e0508e7c0600167aa2f5a1bd7ce2e8b
Khaleel Shaheen <khaleel_sh@hotmail.com>
2021-08-13 03:23:59 +0300

    dispose TextEditingControllers at widget dispose

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -20,6 +20,13 @@ class _LoginPageState extends State<LoginPage> {
   bool _isPasswordValid = false;
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
