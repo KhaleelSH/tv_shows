91d2e5c58fe7515f163c30c23bbc848feeffaef2
Khaleel Shaheen <khaleel_sh@hotmail.com>
2021-08-13 16:57:43 +0300

    add local provider for token storage

---
+++ lib/data/local_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LocalProvider {
+  final FlutterSecureStorage storage;
+
+  LocalProvider({required this.storage});
+
+  static const String _token = 'TOKEN';
+
+  Future<void> setToken(String token) async {
+    await storage.write(key: _token, value: token);
+  }
+
+  Future<void> removeToken() async {
+    await storage.delete(key: _token);
+  }
+
+  Future<String?> getToken() async {
+    return await storage.read(key: _token);
+  }
+}
