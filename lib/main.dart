8c28d35e626c23b8d58bd00c311cad32472451ae
Khaleel Shaheen <khaleel_sh@hotmail.com>
2021-08-14 18:48:14 +0300

    wrap provider around the main App widget to be used later on

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:loggy/loggy.dart';
+import 'package:provider/provider.dart';
+import 'package:tv_shows/data/api_data_client.dart';
 import 'package:tv_shows/pages/login_page.dart';
+import 'package:tv_shows/state/auth_provider.dart';
+import 'package:tv_shows/state/shows_provider.dart';
 import 'package:tv_shows/utils/app_theme.dart';
 
 void main() {
-  Loggy.initLoggy(logPrinter: const PrettyPrinter());
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(showColors: true),
+  );
   runApp(const MyApp());
 }
 
@@ -13,10 +19,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TV Shows',
-      theme: AppTheme.from(theme: AppThemeLight()),
-      home: const LoginPage(),
+    return MultiProvider(
+      providers: [
+        Provider<ApiDataClient>(
+          create: (_) => ApiDataClient(),
+        ),
+        ChangeNotifierProxyProvider<ApiDataClient, AuthProvider>(
+          create: (context) => AuthProvider(context.read<ApiDataClient>()),
+          update: (_, ApiDataClient client, AuthProvider? auth) => auth!,
+        ),
+        ChangeNotifierProxyProvider<ApiDataClient, ShowsProvider>(
+          create: (context) => ShowsProvider(context.read<ApiDataClient>()),
+          update: (_, ApiDataClient client, ShowsProvider? auth) => auth!,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TV Shows',
+        theme: AppTheme.from(theme: AppThemeLight()),
+        home: const LoginPage(),
+      ),
     );
   }
 }
